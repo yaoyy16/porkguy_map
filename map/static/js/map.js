06abe20db99f0ea88f112a69917f79e3bf33fa4a
DSW <dsw41923@gmail.com>
2015-07-24 14:12:26 +0800

    Draw Map

--- map/static/js/map.js
+++ map/static/js/map.js
@@ -1,39 +1,14 @@
 function initialize() {
 	// Create an array of styles.
 	var styles = [{
-		stylers: [
-			{ hue: "#ffa600" },
-			{ saturation: -20 }
-		]
-	},{
 		featureType: "road",
-		elementType: "geometry",
-		stylers: [
-			{ lightness: 50 },
-			{ visibility: "simplified" },
-			{ color: '#ffffff' }
-		]
-	},{
-		featureType: "road",
-		elementType: "labels",
 		stylers: [
 			{ visibility: "off" }
 		]
 	},{
-		featureType: "landscape.natural",
+		featureType: "poi",
 		stylers: [
 			{ "visibility": "off" }
-		]
-	},{
-		featureType: "poi.park",
-		stylers: [
-			{ "visibility": "off" }
-		]
-	},{
-		featureType: "road",
-		elementType: "geometry.fill",
-		stylers: [
-			{ "visibility": "simplified" }
 		]
 	},{
 		featureType: "landscape",
@@ -41,26 +16,26 @@ function initialize() {
 			{ visibility: "off" }
 		]
 	},{
-		featureType: "poi.park",
-		stylers: [
-			{ visibility: "off" }
-		]
-	},{
-		featureType: "road",
-		elementType: "geometry",
-		stylers: [
-			{ "visibility": "off" }
-		]
-	},{
-		featureType: "landscape.natural.terrain",
-		stylers: [
-			{ gamma: 9.99 }
-		]
-	},{
 		featureType: "road",
 		elementType: "labels",
 		stylers: [
 			{ "visibility": "off" }
+		]
+	},{
+		featureType: "water",
+		elementType: "labels",
+		stylers: [
+			{ visibility: "off" }
+		]
+	},{
+		featureType: "administrative",
+		stylers: [
+			{ visibility: "off" }
+		]
+	},{
+		featureType: "transit",
+		stylers: [
+			{ visibility: "off" }
 		]
 	}];
 	// Create a new StyledMapType object, passing it the array of styles,
@@ -70,7 +45,7 @@ function initialize() {
 	// to the map type control.
 	var mapOptions = {
 		mapTypeControlOptions: {
-		mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+			mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
 		}
 	};
 	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
@@ -78,35 +53,17 @@ function initialize() {
 	//Associate the styled map with the MapTypeId and set it to display.
 	map.mapTypes.set('map_style', styledMap);
 	map.setMapTypeId('map_style');
-
-	layer = new google.maps.FusionTablesLayer({
-		query: {
-			select: 'geometry',
-			from: '1ertEwm-1bMBhpEwHhtNYT47HQ9k2ki_6sRa-UQ'
-		},
-		styles: [{
-			polygonOptions: {
-			fillColor: '#00FF00',
-			fillOpacity: 0.3
-			}
-		},{
-			where: 'birds > 300',
-			polygonOptions: {
-				fillColor: '#0000FF'
-			}
-		},{
-			where: 'population > 5',
-			polygonOptions: {
-				fillOpacity: 1.0
-			}
-		}]
-	});
-	layer.setMap(map);
 	var bound1 = new google.maps.LatLng(26.515947, 123.213976);
 	var bound2 = new google.maps.LatLng(21.784257, 118.276845);
 	var bounds = new google.maps.LatLngBounds();
 	bounds.extend(bound1);
 	bounds.extend(bound2);
 	map.fitBounds(bounds);
+	map.data.loadGeoJson('static/bounds.json');
+	var featureStyle = {
+		fillColor: 'brown',
+		strokeWeight: 1
+	}
+	map.data.setStyle(featureStyle);
 }
 google.maps.event.addDomListener(window, 'load', initialize);
