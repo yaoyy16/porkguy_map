e94b8d121629356e24f7535582f2b4d0449e2d21
yaohappy <yaohappy216@gmail.com>
2015-07-30 23:55:09 +0800

    add surplus table

--- apis/admin.py
+++ apis/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from apis.models import City
-# from apis.models import FundGet
-# from apis.models import Surplus
+from apis.models import FundGet
+from apis.models import Surplus
 
 # # Register your models here.
 admin.site.register(City)
-# admin.site.register(FundGet)
-# admin.site.register(Surplus)
+admin.site.register(FundGet)
+admin.site.register(Surplus)
+
--- apis/management/commands/import_surplus.py
+++ apis/management/commands/import_surplus.py
@@ -10,62 +10,66 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
+        def convert(filename):
+            fp = open(filename, 'rb')
+            parser = PDFParser(fp)
+            doc = PDFDocument()
+            parser.set_document(doc)
+            doc.set_parser(parser)
+            doc.initialize('')
+            rsrcmgr = PDFResourceManager()
+            laparams = LAParams()
+            device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+            interpreter = PDFPageInterpreter(rsrcmgr, device)
+            content = ""
+            # Process each page contained in the document.
+            for page in doc.get_pages():
+                interpreter.process_page(page)
+                layout = device.get_result()
+                for lt_obj in layout:
+                    if isinstance(lt_obj, LTTextBox) or isinstance(lt_obj, LTTextLine):
+                        content += lt_obj.get_text()
+            return content
+
+        
         location = ['臺北市', '新北市', '臺中市', '臺南市', '高雄市', '宜蘭縣','桃園縣','新竹縣',
         '苗栗縣', '彰化縣', '南投縣', '雲林縣', '嘉義縣', '屏東縣', '臺東縣', '花蓮縣', '澎湖縣',
         '基隆市', '新竹市', '嘉義市', '金門縣', '連江縣']
 
-        fp = open("./docs/surplus/1031.pdf", 'rb')
-        parser = PDFParser(fp)
-        doc = PDFDocument()
-        parser.set_document(doc)
-        doc.set_parser(parser)
-        doc.initialize('')
-        rsrcmgr = PDFResourceManager()
-        laparams = LAParams()
-        device = PDFPageAggregator(rsrcmgr, laparams=laparams)
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
-        # Process each page contained in the document.
-        for page in doc.get_pages():
-            interpreter.process_page(page)
-            layout = device.get_result()
-            for lt_obj in layout:
-                if isinstance(lt_obj, LTTextBox) or isinstance(lt_obj, LTTextLine):
-                    print(lt_obj.get_text())
-
-        # for i in range(1,13) :
-        #     if i == 12 :
-        #         content = convert('./docs/surplus/103'+str(12)+'.pdf')
-        #         str1 = content.replace(",", "").split("(J+G+H+I)\n\n")
-        #         str2 = str1[1].split("\n\n")[3].split("\n")
-        #         print(i)
-        #         money = []
-        #         for n in str2 :
-        #             money.append(int(n))
-        #         for j in range(0, len(location)) :
-        #             place = location[j]
-        #             print(place)
-        #             city = City.objects.get(name=place) 
-        #             Surplus.objects.create(
-        #                 year = 103,
-        #                 month = i,
-        #                 city = city,
-        #                 surplus = money[j]
-        #             )
-        #     else :
-        #         content = convert('./docs/surplus/103'+str(i)+'.pdf')
-        #         str1 = content.replace(",", "").split("(G+H+I)\n\n")
-        #         str2 = str1[1].split("\n\n")[0].split("\n")
-        #         print(i)
-        #         money = []
-        #         for n in str2 :
-        #             money.append(int(n))
-        #         for j in range(0, len(location)) :
-        #             place = location[j]
-        #             print(place)
-        #             city = City.objects.get(name=place) 
-        #             Surplus.objects.create(
-        #                 year = 103,
-        #                 month = i,
-        #                 city = city,
-        #                 surplus = money[j]
-        #             )
+        for i in range(1,13) :
+            if i == 12 :
+                content = convert('./docs/surplus/103'+str(12)+'.pdf')
+                str1 = content.replace(",", "").split("(G+H+I+J)")
+                str2 = str1[1].split("\n")[23:45]
+                print(i)
+                money = []
+                for n in str2 :
+                    money.append(int(n))
+                for j in range(0, len(location)) :
+                    place = location[j]
+                    print(place)
+                    city = City.objects.get(name=place) 
+                    Surplus.objects.create(
+                        year = 103,
+                        month = i,
+                        city = city,
+                        surplus = money[j]
+                    )
+            else :
+                content = convert('./docs/surplus/103'+str(i)+'.pdf')
+                str1 = content.replace(",", "").split("(G+H+I)\n")
+                str2 = str1[1].split("\n")[0:22]
+                print(i)
+                money = []
+                for n in str2 :
+                    money.append(int(n))
+                for j in range(0, len(location)) :
+                    place = location[j]
+                    print(place)
+                    city = City.objects.get(name=place) 
+                    Surplus.objects.create(
+                        year = 103,
+                        month = i,
+                        city = city,
+                        surplus = money[j]
+                    )
