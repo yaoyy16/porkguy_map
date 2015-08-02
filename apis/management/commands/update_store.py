e15b3fc7485561652604a40105c07c24b247020e
yaohappy <yaohappy216@gmail.com>
2015-08-02 15:29:26 +0800

    update store

--- apis/management/commands/update_store.py
+++ apis/management/commands/update_store.py
@@ -8,14 +8,23 @@ class Command(BaseCommand):
     help = ''
 
     def handle(self, *args, **options):
+        import time 
+
         stores = Lottery_store.objects.all()
+        i = 0
         for store in stores:
             if store.latitude == 0:
-                print(store.name)
+                # print(store.name)
                 url="https://maps.googleapis.com/maps/api/geocode/json?address=%s" % store.address
                 r = requests.get(url).json()["results"][0]["geometry"]
                 lat, lng = r["location"]["lat"], r["location"]["lng"]
                 print(lat, lng)
-                # store.latitude = lat
-                # store.longitude = lng
-                # store.save()
+                store.latitude = lat
+                store.longitude = lng
+                store.save()
+                i +=1
+                if i == 10 :
+                    time.sleep(5)
+                    i = 0
+
+
