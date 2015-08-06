5431deb4a76c6368a9c747bbf62e0ea036596a1b
yaohappy <yaohappy216@gmail.com>
2015-08-06 22:02:42 +0800

    add talking bubble

---
+++ map/static/js/talk.js
@@ -0,0 +1,8 @@
+$("label[for='profit-distribut']").on('mouseover',function () {
+	$('#fundget-box').show();
+	console.log("hahaha");
+});
+$("label[for='profit-distribut']").on('mouseleave',function () {
+	$('#fundget-box').hide();
+	console.log("hahaha");
+});
