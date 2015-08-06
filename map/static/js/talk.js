75be1881672806601935650e1006fd6b6b405b97
george <goblin_george@gmail.com>
2015-08-07 00:20:52 +0800

    update talk

--- map/static/js/talk.js
+++ map/static/js/talk.js
@@ -1,5 +1,11 @@
+function fundget_box_show(msg){
+    var postion_y = $('.pork-guy').position()['top'] + 20;
+    $('#fundget-box').css("margin-top", postion_y).find('.msg').text(msg).end().show();
+}
+
+
 $("label[for='profit-distribut']").on('mouseover',function () {
-	$('#fundget-box').show();
+    fundget_box_show('test');
 	console.log("hahaha");
 });
 $("label[for='profit-distribut']").on('mouseleave',function () {
