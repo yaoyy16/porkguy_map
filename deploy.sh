713cb6a00a59cf2da906a269e1ffe9e5504251d4
lucemia <lucemia@gmail.com>
2016-01-23 03:44:24 +0800

    ignore error

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e
 REPO=porkguy
 
 docker login --username=$DOCKER_USER --password=$DOCKER_PASS --email=$DOCKER_EMAIL
-docker pull lucemia/$REPO
+docker pull lucemia/$REPO || true
 docker build -t $REPO .
 docker tag $REPO lucemia/$REPO
 docker push lucemia/$REPO
