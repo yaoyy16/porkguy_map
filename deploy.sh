d27015d93a82ed7bfe54bff5b0ec26cba9e5ffe5
lucemia <lucemia@gmail.com>
2016-01-24 11:28:05 +0800

    improve secuity

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@ set -e
 
 REPO=porkguy
 
-docker login --username=$DOCKER_USER --password=$DOCKER_PASS --email=$DOCKER_EMAIL
 docker pull lucemia/$REPO || true
 docker build -t $REPO .
 docker tag -f $REPO lucemia/$REPO
