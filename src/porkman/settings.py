636640d02d06e44bfffd727aa7e163c20366cb70
george <goblin_george@gmail.com>
2016-03-14 23:04:25 +0800

    update settings

--- src/porkman/settings.py
+++ src/porkman/settings.py
@@ -96,7 +96,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = 'http://porkman.cdn.gliacloud.com/static/'
+STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
